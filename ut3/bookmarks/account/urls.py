1884039957166ca5c4ed2dd2b7e7b8819d25297e
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-10-15 11:51:48 +0100

    Add code for chapter 4

---
+++ ut3/bookmarks/account/urls.py
@@ -0,0 +1,11 @@
+# from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
