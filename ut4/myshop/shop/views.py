19a88fd614e9add81984a66793d5be11d4b1acb0
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-10-27 10:59:44 +0100

    Add code for chapter 8

---
+++ ut4/myshop/shop/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import get_object_or_404, render
+
+from cart.forms import CartAddProductForm
+
+from .models import Category, Product
+
+
+def product_list(request, category_slug: str = None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(
+        request,
+        'shop/product/list.html',
+        {'category': category, 'categories': categories, 'products': products},
+    )
+
+
+def product_detail(request, id: int, slug: str):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(
+        request,
+        'shop/product/detail.html',
+        {'product': product, 'cart_product_form': cart_product_form},
+    )
