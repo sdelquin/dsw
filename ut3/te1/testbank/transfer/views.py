fd501fc962690befe7af902499a4613a59cd7ad9
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-21 16:59:44 +0000

    Add error checking

--- ut3/te1/testbank/transfer/views.py
+++ ut3/te1/testbank/transfer/views.py
@@ -1,5 +1,22 @@
+import json
+import re
+
+from django.http import HttpResponseBadRequest
 from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def incoming_transfer(request):
-    return HttpResponse('holi')
+    BANK_ID = 0
+    REQUIRED_FIELDS = ('sender', 'cac', 'concept', 'amount')
+
+    data = json.loads(request.body)
+    for field in REQUIRED_FIELDS:
+        if field not in data:
+            return HttpResponseBadRequest(f'Field "{field}" not in request')
+        if not data[field]:
+            return HttpResponseBadRequest(f'Field "{field}" has no value')
+    if not re.fullmatch(rf'A{BANK_ID}-\d{{4}}', cac := data['cac']):
+        return HttpResponseBadRequest(f'CAC "{cac}" does not exist in our bank')
+    return HttpResponse('✅ Transfer successfully processed')
