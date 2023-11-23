5d60df8e9ffa9fb116a294bd2b29c017a3be10e9
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-23 11:06:06 +0000

    Add fixtures to tests

--- ut3/te1/testbank/test_e2e.py
+++ ut3/te1/testbank/test_e2e.py
@@ -1,42 +1,51 @@
+import pytest
 import requests
 
-BANK_URL = 'http://localhost:8000/'
-TRANSFER_URL = BANK_URL + 'transfer/incoming/'
-PAYLOAD = dict(sender='Google Inc.', cac='A0-0001', concept='Invoice 341', amount='500')
+
+@pytest.fixture
+def transfer_url():
+    BANK_URL = 'http://localhost:8000/'
+    return BANK_URL + 'transfer/incoming/'
 
 
-def test_transfer_works_fine():
-    response = requests.post(TRANSFER_URL, json=PAYLOAD)
+@pytest.fixture
+def payload():
+    return dict(sender='Google Inc.', cac='A0-0001', concept='Invoice 341', amount='500')
+
+
+def test_transfer_works_fine(transfer_url, payload):
+    response = requests.post(transfer_url, json=payload)
     assert response.status_code == 200
     assert response.text == '✅ Transfer successfully processed'
 
 
-def test_transfer_fails_when_missing_fields():
-    payload = PAYLOAD.copy()
-    response = requests.post(TRANSFER_URL, json=payload.pop('sender'))
+def test_transfer_fails_when_missing_fields(transfer_url, payload):
+    response = requests.post(transfer_url, json=payload.pop('sender'))
     assert response.status_code == 400
     assert response.text == 'Field "sender" not in request'
 
 
-def test_transfer_fails_when_empty_values():
-    payload = PAYLOAD.copy()
+def test_transfer_fails_when_empty_values(transfer_url, payload):
     payload['sender'] = '  '
-    response = requests.post(TRANSFER_URL, json=payload)
+    response = requests.post(transfer_url, json=payload)
     assert response.status_code == 400
     assert response.text == 'Field "sender" has no value'
 
 
-def test_transfer_fails_when_cac_does_not_exist():
-    payload = PAYLOAD.copy()
+def test_transfer_fails_when_cac_does_not_exist(transfer_url, payload):
     payload['cac'] = 'B0-0001'
-    response = requests.post(TRANSFER_URL, json=payload)
+    response = requests.post(transfer_url, json=payload)
     assert response.status_code == 400
     assert response.text == 'CAC "B0-0001" does not exist in our bank'
 
 
-def test_transfer_fails_when_amount_is_not_decimal():
-    payload = PAYLOAD.copy()
+def test_transfer_fails_when_amount_is_not_decimal(transfer_url, payload):
     payload['amount'] = 'hello'
-    response = requests.post(TRANSFER_URL, json=payload)
+    response = requests.post(transfer_url, json=payload)
     assert response.status_code == 400
     assert response.text == 'Invalid value "hello" for field "amount": decimal value expected'
+
+
+def test_transfer_fails_when_request_is_not_post(transfer_url, payload):
+    response = requests.get(transfer_url, json=payload)
+    assert response.status_code == 405
