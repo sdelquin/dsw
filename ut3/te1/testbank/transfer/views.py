fc5df71cf2cd5a2b9c2cc2bab4a737b34cc1aae0
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-23 09:54:46 +0000

    Strip input values to check contents

--- ut3/te1/testbank/transfer/views.py
+++ ut3/te1/testbank/transfer/views.py
@@ -16,12 +16,14 @@ def incoming_transfer(request):
     for field in REQUIRED_FIELDS:
         if field not in data:
             return HttpResponseBadRequest(f'Field "{field}" not in request')
-        if not data[field]:
+        if not data[field].strip():
             return HttpResponseBadRequest(f'Field "{field}" has no value')
     if not re.fullmatch(rf'A{BANK_ID}-\d{{4}}', cac := data['cac']):
         return HttpResponseBadRequest(f'CAC "{cac}" does not exist in our bank')
     try:
         Decimal(amount := data['amount'])
     except InvalidOperation:
-        return HttpResponseBadRequest(f'Invalid value "{amount}" for field "amount"')
+        return HttpResponseBadRequest(
+            f'Invalid value "{amount}" for field "amount": decimal value expected'
+        )
     return HttpResponse('✅ Transfer successfully processed')
