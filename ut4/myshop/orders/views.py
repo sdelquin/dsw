ef85538d34fdea18f6fd20538f77571490b202c7
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-27 12:25:45 +0000

    Fix issues related with static files

--- ut4/myshop/orders/views.py
+++ ut4/myshop/orders/views.py
@@ -1,6 +1,6 @@
 import weasyprint
-from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.staticfiles import finders
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
@@ -56,6 +56,6 @@ def admin_order_pdf(request, order_id):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
     weasyprint.HTML(string=html).write_pdf(
-        response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
+        response, stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))]
     )
     return response
