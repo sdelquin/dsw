b7c0f9b875c45271cbc1ae51c5e0c1b263f4c5e4
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-10-22 19:10:38 +0100

    Fix field names

--- ut2/testing/tests/test_project.py
+++ ut2/testing/tests/test_project.py
@@ -16,8 +16,8 @@ def task():
 def test_task_model_has_proper_fields(client: Client, task):
     PROPER_FIELDS = (
         'id',
-        'title',
-        'content',
+        'name',
+        'description',
         'slug',
         'done',
         'complete_before',
@@ -45,7 +45,7 @@ def test_list_all_tasks(client: Client):
     response = client.get('/tasks/')
     assert response.status_code == 200
     for task in tasks:
-        assertContains(response, task.title)
+        assertContains(response, task.name)
         assertContains(response, DETAIL_URL.format(slug=task.slug))
         assertContains(response, TOGGLE_URL.format(slug=task.slug))
         assertContains(response, EDIT_URL.format(slug=task.slug))
@@ -58,7 +58,7 @@ def test_list_pending_tasks(client: Client):
     response = client.get('/tasks/pending/')
     assert response.status_code == 200
     for task in tasks:
-        assertContains(response, task.title)
+        assertContains(response, task.name)
 
 
 @pytest.mark.django_db
@@ -67,7 +67,7 @@ def test_list_done_tasks(client: Client):
     response = client.get('/tasks/done/')
     assert response.status_code == 200
     for task in tasks:
-        assertContains(response, task.title)
+        assertContains(response, task.name)
 
 
 @pytest.mark.django_db
@@ -75,8 +75,8 @@ def test_task_detail(client: Client, task):
     url = f'/tasks/task/{task.slug}/'
     response = client.get(url)
     assert response.status_code == 200
-    assertContains(response, task.title)
-    assertContains(response, task.content)
+    assertContains(response, task.name)
+    assertContains(response, task.description)
     assert response.context['task'] == task
 
 
@@ -94,8 +94,8 @@ def test_add_task_with_get_method(client: Client):
     response = client.get('/tasks/add/')
     assert response.status_code == 200
     assert response.context['form']
-    assert 'title' in response.context['form'].fields
-    assert 'content' in response.context['form'].fields
+    assert 'name' in response.context['form'].fields
+    assert 'description' in response.context['form'].fields
     assert 'slug' not in response.context['form'].fields
     assert 'done' not in response.context['form'].fields
 
@@ -103,13 +103,15 @@ def test_add_task_with_get_method(client: Client):
 @pytest.mark.django_db
 def test_add_task_with_post_method(client: Client):
     task = baker.prepare(Task, _fill_optional=True)
-    payload = dict(title=task.title, content=task.content, complete_before=task.complete_before)
+    payload = dict(
+        name=task.name, description=task.description, complete_before=task.complete_before
+    )
     response = client.post('/tasks/add/', payload)
     assertRedirects(response, '/tasks/')
     latest_task = Task.objects.latest('pk')
-    assert latest_task.title == task.title
-    assert latest_task.slug == slugify(task.title)
-    assert latest_task.content == task.content
+    assert latest_task.name == task.name
+    assert latest_task.slug == slugify(task.name)
+    assert latest_task.description == task.description
     assert latest_task.complete_before == task.complete_before
     assert latest_task.created_at is not None
     assert latest_task.updated_at is not None
@@ -119,7 +121,7 @@ def test_add_task_with_post_method(client: Client):
 def test_add_task_fails_when_missing_required_fields(client: Client):
     response = client.post('/tasks/add/')
     assert response.status_code == 200
-    assertFormError(response.context['form'], 'title', 'This field is required.')
+    assertFormError(response.context['form'], 'name', 'This field is required.')
 
 
 @pytest.mark.django_db
@@ -128,16 +130,16 @@ def test_edit_task_with_get_method(client: Client, task):
     response = client.get(url)
     assert response.status_code == 200
     form_values = response.context['form'].initial.values()
-    assert task.title in form_values
-    assert task.content in form_values
+    assert task.name in form_values
+    assert task.description in form_values
 
 
 @pytest.mark.django_db
 def test_edit_task_with_post_method(client: Client, task):
     form_task = baker.prepare(Task, _fill_optional=True)
     payload = dict(
-        title=form_task.title,
-        content=form_task.content,
+        name=form_task.name,
+        description=form_task.description,
         done=form_task.done,
         complete_before=form_task.complete_before,
     )
@@ -145,9 +147,9 @@ def test_edit_task_with_post_method(client: Client, task):
     response = client.post(url, payload)
     assert response.status_code in [200, 302]
     edited_task = Task.objects.get(pk=task.pk)
-    assert edited_task.title == form_task.title
-    assert edited_task.slug == slugify(form_task.title)
-    assert edited_task.content == form_task.content
+    assert edited_task.name == form_task.name
+    assert edited_task.slug == slugify(form_task.name)
+    assert edited_task.description == form_task.description
     assert edited_task.done == form_task.done
     assert edited_task.complete_before == form_task.complete_before
     assert edited_task.updated_at > edited_task.created_at
@@ -158,7 +160,7 @@ def test_edit_task_fails_when_missing_required_fields(client: Client, task):
     url = f'/tasks/task/{task.slug}/edit/'
     response = client.post(url)
     assert response.status_code == 200
-    assertFormError(response.context['form'], 'title', 'This field is required.')
+    assertFormError(response.context['form'], 'name', 'This field is required.')
 
 
 @pytest.mark.django_db
